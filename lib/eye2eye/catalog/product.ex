9fc4f56e0d65497909b0a1beb1048be0709debda
fi-ya <safiaali@hotmail.com>
2022-08-25 11:53:58 +0100

    add product schema

---
+++ lib/eye2eye/catalog/product.ex
@@ -0,0 +1,19 @@
+defmodule Eye2eye.Catalog.Product do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  schema "products" do
+    field :image_url, :string
+    field :name, :string
+    field :price, :decimal
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(product, attrs) do
+    product
+    |> cast(attrs, [:name, :image_url, :price])
+    |> validate_required([:name, :image_url, :price ])
+  end
+end
