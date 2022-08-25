7e88c86b369efadc4cacdf7a2575f8837b62e21b
fi-ya <safiaali@hotmail.com>
2022-08-25 12:01:00 +0100

    test catalog context querying database

---
+++ test/support/fixtures/catalog_fixtures.ex
@@ -0,0 +1,22 @@
+defmodule Eye2eye.CatalogFixtures do
+  @moduledoc """
+  This module defines test helpers for creating
+  entities via the `Eye2eye.Catalog` context.
+  """
+
+  @doc """
+  Generate a product.
+  """
+  def product_fixture(attrs \\ %{}) do
+    {:ok, product} =
+      attrs
+      |> Enum.into(%{
+        image_url: "some image_url",
+        name: "some name",
+        price: "120.50",
+      })
+      |> Eye2eye.Catalog.create_product()
+
+    product
+  end
+end
