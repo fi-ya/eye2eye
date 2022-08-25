e933e6f5f30818c26089ed1ba74aba6113f3b288
fi-ya <safiaali@hotmail.com>
2022-08-25 12:54:56 +0100

    Remove used imports

--- test/eye2eye_web/controllers/product_controller_test.exs
+++ test/eye2eye_web/controllers/product_controller_test.exs
@@ -1,12 +1,8 @@
 defmodule Eye2eyeWeb.ProductControllerTest do
   use Eye2eyeWeb.ConnCase
 
-  alias Eye2eye.Catalog
-
-  import Eye2eye.CatalogFixtures
-
   describe "index" do
-    test "lists all ", %{conn: conn} do
+    test "lists all products", %{conn: conn} do
       conn = get(conn, Routes.product_path(conn, :index))
       assert html_response(conn, 200) =~ "Summer 2022 Collection"
     end
