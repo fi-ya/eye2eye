84eba7224e723e559ff1446a68f1eb470aa2470e
fi-ya <safiaali@hotmail.com>
2022-08-26 10:00:36 +0100

    remove unused methods in catalog context

--- test/eye2eye/catalog_test.exs
+++ test/eye2eye/catalog_test.exs
@@ -15,11 +15,6 @@ defmodule Eye2eye.CatalogTest do
       assert Catalog.list_products() == [product]
     end
 
-    test "get_product!/1 returns the product with given id" do
-      product = product_fixture()
-      assert Catalog.get_product!(product.id) == product
-    end
-
     test "create_product/1 with valid data creates a product" do
       valid_attrs = %{
         image_url: "https://images.unsplash.com/photo",
@@ -35,38 +30,6 @@ defmodule Eye2eye.CatalogTest do
 
     test "create_product/1 with invalid data returns error changeset" do
       assert {:error, %Ecto.Changeset{}} = Catalog.create_product(@invalid_attrs)
-    end
-
-    test "update_product/2 with valid data updates the product" do
-      product = product_fixture()
-
-      update_attrs = %{
-        image_url: "https://images.unsplash.com/photo2",
-        name: "some updated name",
-        price: "456.70"
-      }
-
-      assert {:ok, %Product{} = product} = Catalog.update_product(product, update_attrs)
-      assert product.image_url == "https://images.unsplash.com/photo2"
-      assert product.name == "some updated name"
-      assert product.price == Decimal.new("456.70")
-    end
-
-    test "update_product/2 with invalid data returns error changeset" do
-      product = product_fixture()
-      assert {:error, %Ecto.Changeset{}} = Catalog.update_product(product, @invalid_attrs)
-      assert product == Catalog.get_product!(product.id)
-    end
-
-    test "delete_product/1 deletes the product" do
-      product = product_fixture()
-      assert {:ok, %Product{}} = Catalog.delete_product(product)
-      assert_raise Ecto.NoResultsError, fn -> Catalog.get_product!(product.id) end
-    end
-
-    test "change_product/1 returns a product changeset" do
-      product = product_fixture()
-      assert %Ecto.Changeset{} = Catalog.change_product(product)
     end
   end
 end
