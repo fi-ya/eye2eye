bb04a14b8e261baa5c9f2e9f90e943a776c46b5e
fi-ya <safiaali@hotmail.com>
2022-09-05 10:27:28 +0100

    add calculations for total cart items, total item price and total cart price
    add tests and add shopping fixture methods

--- lib/eye2eye/shopping_cart.ex
+++ lib/eye2eye/shopping_cart.ex
@@ -31,35 +31,6 @@ defmodule Eye2eye.ShoppingCart do
   end
 
   @doc """
-  Returns the list of carts.
-
-  ## Examples
-
-      iex> list_carts()
-      [%Cart{}, ...]
-
-  """
-  def list_carts do
-    Repo.all(Cart)
-  end
-
-  @doc """
-  Gets a single cart.
-
-  Raises `Ecto.NoResultsError` if the Cart does not exist.
-
-  ## Examples
-
-      iex> get_cart!(123)
-      %Cart{}
-
-      iex> get_cart!(456)
-      ** (Ecto.NoResultsError)
-
-  """
-  def get_cart!(id), do: Repo.get!(Cart, id)
-
-  @doc """
   Creates a cart using user uuid.
 
   If insert successful reload cart contents
@@ -83,14 +54,7 @@ defmodule Eye2eye.ShoppingCart do
     end
   end
 
-  defp reload_cart(%Cart{} = cart), do: get_cart_by_user_uuid(cart.user_uuid)
-
-  def total_cart_items(%Cart{} = cart) do
-    Enum.reduce(cart.items, 0, fn item, acc ->
-      item.quantity
-      |> Decimal.add(acc)
-    end)
-  end
+  def reload_cart(%Cart{} = cart), do: get_cart_by_user_uuid(cart.user_uuid)
 
   @doc """
   Updates a cart.
@@ -104,11 +68,12 @@ defmodule Eye2eye.ShoppingCart do
       {:error, %Ecto.Changeset{}}
 
   """
-  def update_cart(%Cart{} = cart, attrs) do
-    cart
-    |> Cart.changeset(attrs)
-    |> Repo.update()
-  end
+
+  # def update_cart(%Cart{} = cart, attrs) do
+  #   cart
+  #   |> Cart.changeset(attrs)
+  #   |> Repo.update()
+  # end
 
   @doc """
   Deletes a cart.
@@ -122,9 +87,10 @@ defmodule Eye2eye.ShoppingCart do
       {:error, %Ecto.Changeset{}}
 
   """
-  def delete_cart(%Cart{} = cart) do
-    Repo.delete(cart)
-  end
+
+  # def delete_cart(%Cart{} = cart) do
+  #   Repo.delete(cart)
+  # end
 
   @doc """
   Returns an `%Ecto.Changeset{}` for tracking cart changes.
@@ -135,9 +101,26 @@ defmodule Eye2eye.ShoppingCart do
       %Ecto.Changeset{data: %Cart{}}
 
   """
-  def change_cart(%Cart{} = cart, attrs \\ %{}) do
-    Cart.changeset(cart, attrs)
+
+  # def change_cart(%Cart{} = cart, attrs \\ %{}) do
+  #   Cart.changeset(cart, attrs)
+  # end
+
+  def total_cart_items(%Cart{} = cart) do
+    Enum.reduce(cart.items, 0, fn item, acc ->
+      item.quantity + acc
+    end)
   end
+
+  def total_cart_price(%Cart{} = cart) do
+    Enum.reduce(cart.items, Decimal.new("0.00"), fn item, acc ->
+      item
+      |> total_item_price()
+      |> Decimal.add(acc)
+    end)
+  end
+
+  # ----- CART ITEMS FUNCTIONS START HERE -----
 
   @doc """
   Returns the list of cart_items.
@@ -148,9 +131,10 @@ defmodule Eye2eye.ShoppingCart do
       [%CartItem{}, ...]
 
   """
-  def list_cart_items do
-    Repo.all(CartItem)
-  end
+
+  # def list_cart_items do
+  #   Repo.all(CartItem)
+  # end
 
   @doc """
   Gets a single cart_item.
@@ -166,25 +150,8 @@ defmodule Eye2eye.ShoppingCart do
       ** (Ecto.NoResultsError)
 
   """
-  def get_cart_item!(id), do: Repo.get!(CartItem, id)
 
-  @doc """
-  Creates a cart_item.
-
-  ## Examples
-
-      iex> create_cart_item(%{field: value})
-      {:ok, %CartItem{}}
-
-      iex> create_cart_item(%{field: bad_value})
-      {:error, %Ecto.Changeset{}}
-
-  """
-  def create_cart_item(attrs \\ %{}) do
-    %CartItem{}
-    |> CartItem.changeset(attrs)
-    |> Repo.insert()
-  end
+  # def get_cart_item!(id), do: Repo.get!(CartItem, id)
 
   @doc """
   Adds an item to cart.
@@ -202,6 +169,12 @@ defmodule Eye2eye.ShoppingCart do
       on_conflict: [inc: [quantity: 1]],
       conflict_target: [:cart_id, :product_id]
     )
+
+    {:ok, reload_cart(cart)}
+  end
+
+  def total_item_price(%CartItem{} = item) do
+    Decimal.mult(item.product.price, item.quantity)
   end
 
   @doc """
@@ -216,11 +189,12 @@ defmodule Eye2eye.ShoppingCart do
       {:error, %Ecto.Changeset{}}
 
   """
-  def update_cart_item(%CartItem{} = cart_item, attrs) do
-    cart_item
-    |> CartItem.changeset(attrs)
-    |> Repo.update()
-  end
+
+  # def update_cart_item(%CartItem{} = cart_item, attrs) do
+  #   cart_item
+  #   |> CartItem.changeset(attrs)
+  #   |> Repo.update()
+  # end
 
   @doc """
   Deletes a cart_item.
@@ -234,9 +208,10 @@ defmodule Eye2eye.ShoppingCart do
       {:error, %Ecto.Changeset{}}
 
   """
-  def delete_cart_item(%CartItem{} = cart_item) do
-    Repo.delete(cart_item)
-  end
+
+  # def delete_cart_item(%CartItem{} = cart_item) do
+  #   Repo.delete(cart_item)
+  # end
 
   @doc """
   Returns an `%Ecto.Changeset{}` for tracking cart_item changes.
@@ -247,7 +222,7 @@ defmodule Eye2eye.ShoppingCart do
       %Ecto.Changeset{data: %CartItem{}}
 
   """
-  def change_cart_item(%CartItem{} = cart_item, attrs \\ %{}) do
-    CartItem.changeset(cart_item, attrs)
-  end
+  # def change_cart_item(%CartItem{} = cart_item, attrs \\ %{}) do
+  #   CartItem.changeset(cart_item, attrs)
+  # end
 end
