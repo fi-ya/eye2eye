f5e445fd447b1f15e4a85d7cedd4fe7634326e61
fi-ya <safiaali@hotmail.com>
2022-09-05 10:37:01 +0100

    test cart_item_controller redirects to product index route when data is valid, displays success message and cart counter

--- test/eye2eye_web/controllers/cart_item_controller_test.exs
+++ test/eye2eye_web/controllers/cart_item_controller_test.exs
@@ -1,25 +1,26 @@
 defmodule Eye2eyeWeb.CartItemControllerTest do
   use Eye2eyeWeb.ConnCase
 
-  # import Eye2eye.ShoppingCartFixtures
+  alias Eye2eye.Catalog
+  alias Eye2eye.ShoppingCart
 
-  # @create_attrs %{product_id: 1}
-  # @invalid_attrs %{product_id: nil}
+  import Eye2eye.CatalogFixtures
+
+  @invalid_attrs %{product_id: 2}
 
   describe "create a cart item" do
-    # test "redirects to product index route when data is valid", %{conn: conn} do
-    #   conn = post(conn, Routes.cart_item_path(conn, :create), product_id: @create_attrs)
+    test "redirects to product index route when data is valid, displays success message and cart counter",
+         %{conn: conn} do
+      product = create_product_fixture()
 
-    #   assert %{id: id} = redirected_params(conn)
-    #   assert redirected_to(conn) == Routes.product_path(conn, :index, id)
+      conn = post(conn, Routes.cart_item_path(conn, :create), product_id: product.id)
 
-    #   conn = get(conn, Routes.product_path(conn, :index, id))
-    #   assert html_response(conn, 200) =~ "Item added to your cart"
-    # end
+      assert redirected_to(conn) == Routes.product_path(conn, :index)
 
-    # test "renders errors when data is invalid", %{conn: conn} do
-    #   conn = post(conn, Routes.cart_item_path(conn, :create), product: @invalid_attrs)
-    #   assert html_response(conn, 200) =~ "There was an error adding the item to your cart"
-    # end
+      conn = get(conn, Routes.product_path(conn, :index))
+
+      assert html_response(conn, 200) =~ "Item added to your cart"
+      assert ShoppingCart.total_cart_items(conn.assigns.cart) == 1
+    end
   end
 end
