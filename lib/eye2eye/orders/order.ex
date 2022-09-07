5839216ad9ecb47d23e65f99ec30442dcdd9ce92
fi-ya <safiaali@hotmail.com>
2022-09-07 12:13:23 +0100

    create orders context, schema order, table orders with fields total price and user_uuid, with migration

---
+++ lib/eye2eye/orders/order.ex
@@ -0,0 +1,18 @@
+defmodule Eye2eye.Orders.Order do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  schema "orders" do
+    field :total_price, :decimal
+    field :user_uuid, Ecto.UUID
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(order, attrs) do
+    order
+    |> cast(attrs, [:user_uuid, :total_price])
+    |> validate_required([:user_uuid, :total_price])
+  end
+end
