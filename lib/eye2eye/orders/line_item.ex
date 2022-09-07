df0b6256199cd05dcb2ce39f33e755e0281bd0ec
fi-ya <safiaali@hotmail.com>
2022-09-07 12:33:11 +0100

    generate LineItem schema in Orders context, with fields for price, quantity and references to order_id & product_id

---
+++ lib/eye2eye/orders/line_item.ex
@@ -0,0 +1,20 @@
+defmodule Eye2eye.Orders.LineItem do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  schema "order_line_items" do
+    field :price, :decimal
+    field :quantity, :integer
+    field :order_id, :id
+    field :product_id, :id
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(line_item, attrs) do
+    line_item
+    |> cast(attrs, [:price, :quantity])
+    |> validate_required([:price, :quantity])
+  end
+end
