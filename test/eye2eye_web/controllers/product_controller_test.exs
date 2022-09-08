9792b6a80ae9bf12d220a287d352fc3cbd194039
fi-ya <safiaali@hotmail.com>
2022-09-08 13:38:09 +0100

    refactor ProductController test, add setup method

--- test/eye2eye_web/controllers/product_controller_test.exs
+++ test/eye2eye_web/controllers/product_controller_test.exs
@@ -6,17 +6,21 @@ defmodule Eye2eyeWeb.ProductControllerTest do
   import Eye2eye.CatalogFixtures
 
   describe "index" do
-    test "lists all products", %{conn: conn} do
-      product = create_product_fixture()
+    setup [:create_product]
 
+    test "lists all products", %{conn: conn} do
       conn = get(conn, Routes.product_path(conn, :index))
 
       assert html_response(conn, 200) =~ "Summer 2022 Collection"
       assert html_response(conn, 200) =~ "Product One"
       assert html_response(conn, 200) =~ "https://images.com/1"
       assert html_response(conn, 200) =~ "120.50"
-      assert html_response(conn, 200) != "Product Two"
-      assert html_response(conn, 200) != "Add to cart"
+      assert html_response(conn, 200) =~ "Add to cart"
     end
+  end
+
+  defp create_product(_) do
+    product = create_product_fixture()
+    %{product: product}
   end
 end
