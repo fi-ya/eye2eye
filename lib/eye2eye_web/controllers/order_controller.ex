12c9fe465349920a6782a60e0efcec13d12cfe14
fi-ya <safiaali@hotmail.com>
2022-09-08 13:01:46 +0100

    remove unused imports

--- lib/eye2eye_web/controllers/order_controller.ex
+++ lib/eye2eye_web/controllers/order_controller.ex
@@ -2,12 +2,10 @@ defmodule Eye2eyeWeb.OrderController do
   use Eye2eyeWeb, :controller
 
   alias Eye2eye.Orders
-  alias Eye2eye.Orders.Order
-  alias Eye2eye.{ShoppingCart, Catalog}
 
   def create(conn, %{"order" => order_params}) do
     case Orders.complete_order(conn.assigns.cart, order_params) do
-      {:ok, order} ->
+      {:ok, _order} ->
         conn
         |> put_flash(:info, "Order created successfully.")
         |> redirect(to: Routes.cart_path(conn, :show))
