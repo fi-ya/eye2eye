3320b818aeff4c99895738382e4f7e1b010b2ba7
fi-ya <safiaali@hotmail.com>
2022-09-09 11:53:10 +0100

    run formatter

--- lib/eye2eye_web/views/order_view.ex
+++ lib/eye2eye_web/views/order_view.ex
@@ -4,23 +4,25 @@ defmodule Eye2eyeWeb.OrderView do
   def convert_naive_to_date_str(naive_date_time) do
     date_str =
       naive_date_time
-       |> NaiveDateTime.to_date()
-       |> Date.to_string()
-   end
+      |> NaiveDateTime.to_date()
+      |> Date.to_string()
+  end
 
-   def format_date(date_str) do
-      date_list = String.split(date_str, "-")
+  def format_date(date_str) do
+    date_list = String.split(date_str, "-")
 
-      formatted_date =  Enum.at(date_list, 2) <> "-" <> Enum.at(date_list, 1) <> "-" <> Enum.at(date_list, 0)
-      formatted_date
-    end
+    formatted_date =
+      Enum.at(date_list, 2) <> "-" <> Enum.at(date_list, 1) <> "-" <> Enum.at(date_list, 0)
 
-    def format_naive_to_date(naive_date_time) do
-      date =
+    formatted_date
+  end
+
+  def format_naive_to_date(naive_date_time) do
+    date =
       naive_date_time
-        |> convert_naive_to_date_str()
-        |> format_date()
-      date
-    end
+      |> convert_naive_to_date_str()
+      |> format_date()
 
+    date
+  end
 end
